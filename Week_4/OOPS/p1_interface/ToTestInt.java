cecc6d585f8b1d696780ed7500071382f84af84b
Rachit Singhal <rachitsinghal106@gmail.com>
2026-06-28 14:47:34 +0530

    Create ToTestInt.java

---
+++ Week_4/OOPS/p1_interface/ToTestInt.java
@@ -0,0 +1,8 @@
+package p1_interface;
+
+public class ToTestInt {
+    public static void main(String[] args) {
+        Arithmetic obj = new Arithmetic();
+        System.out.println("Square = " + obj.square(5));
+    }
+}
